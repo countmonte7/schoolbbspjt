6345e67f6d31f6d4533a0f8e205d0398e63cade1
countmonte7 <countmonte7@naver.com>
2019-07-14 21:08:14 +0900

    fixed DB confidentials

--- src/app/model/DB.java
+++ src/app/model/DB.java
@@ -1,7 +1,9 @@
 package app.model;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 import javax.naming.Context;
@@ -11,12 +13,14 @@ public class DB {
 	public Connection doConnect() {
 		Connection conn = null;
 	    try {
-	    	String id ="root";
-	  		String pw="47690eo5dh";
+	    	Properties prop = new Properties();
+			FileInputStream ip = new FileInputStream("C:/spring/schoolboard/config.properties");
+			prop.load(ip);
+			String id = prop.getProperty("dbId");
+			String pw = prop.getProperty("dbPw");
 	  		Class.forName("com.mysql.cj.jdbc.Driver");
 	  		String url="jdbc:mysql://localhost:3306/schoolboard?serverTimezone=UTC";
 	  		conn = DriverManager.getConnection(url, id, pw);
-	    	
 	    }catch(Exception e) {
 	    	e.printStackTrace();
 	    }
